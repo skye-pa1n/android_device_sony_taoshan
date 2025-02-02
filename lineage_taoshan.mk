a3964a0deb3c238aca97e192f2dcca037f68e7b0
skye // pa1n <sonicgames772@gmail.com>
2025-02-02 17:09:09 -0500

    taoshan: BT MACADR patch try to fix BT

--- lineage_taoshan.mk
+++ lineage_taoshan.mk
@@ -32,9 +32,6 @@ PRODUCT_BRAND := Sony
 PRODUCT_MANUFACTURER := Sony
 PRODUCT_MODEL := Xperia L
 
-# Init a dummy BT MAC address, will be overwritten later
-BOARD_BT_MAC_ADDR := "00:00:00:00:00:00"
-
 # Build fingerprints
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=C2105 \
