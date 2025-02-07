7235af05d85b05e46eb2517a9a1eb3635cd75f1c
@skye // pa1n <sonicgames772@gmail.com>
2025-02-07 16:03:50 -0500

    Update device.mk

--- device.mk
+++ device.mk
@@ -33,11 +33,11 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 # Product common configurations
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit common Android Go configurations
-$(call inherit-product, build/target/product/go_defaults.mk)
+# Inherit Android Go & Team-NoobMaster69 configurations
+$(call inherit-product, build/target/product/go_defaults_512.mk)
 
 # Call some extra configs picked from Team-NoobMaster69
-$(call inherit-product, device/extras/memory/product.mk)
+#$(call inherit-product, device/extras/memory/product.mk)
 
 # Device product elements
 include $(LOCAL_PATH)/product/*.mk
