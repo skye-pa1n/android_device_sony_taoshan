49f320ca67d7005fe3edbf641427229512de608f
Daniel Jarai <jaraidaniel@gmail.com>
2026-03-11 15:47:44 +0000

    camera: Specify internal video devices to ignore by the external provider The external camera provider occupies our camera V4L2 nodes, potentially crashing the kernel driver and blocking the camera HAL. Unfortunately, there's no easy way to just disable it, so let's just tell it to ignore the internal video devices.
    Change-Id: I00c67de1b94a1f7eca1bf458d1d0e4de3507aa81
    
    Signed-off-by: Daniel Jarai <jaraidaniel@gmail.com>
    Signed-off-by: skye-tachyon <sonicgames772@gmail.com>

--- product/camera.mk
+++ product/camera.mk
@@ -1,8 +1,19 @@
 # Camera packages
 PRODUCT_PACKAGES += \
-    camera.qcom
+    camera.qcom \
+    Snap
+
+# Camera SHIM packages
+PRODUCT_PACKAGES += \
+    libshim_cald \
+    libshim_camera
+
+# Camera configurations
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml
 
 # Camera properties
 PRODUCT_PROPERTY_OVERRIDES += \
     camera2.portability.force_api=1 \
-    camera.disable_treble=true
+    media.stagefright.legacyencoder=true \
+    media.stagefright.less-secure=true
