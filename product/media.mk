1d76ffa9ee8a82a9189496f05a7e01291e0d735d
skye-tachyon <sonicgames772@gmail.com>
2026-03-12 15:15:38 +0000

    media: remove media_profiles.xml
    Signed-off-by: skye-tachyon <sonicgames772@gmail.com>

--- product/media.mk
+++ product/media.mk
@@ -1,8 +1,7 @@
 # Media configurations
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
-    $(LOCAL_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
-    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
+    $(LOCAL_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml
 
 # Media properties
 PRODUCT_PROPERTY_OVERRIDES += \
