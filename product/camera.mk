88149dcb65b1293687822512e3981c1d1ae0ad31
skye-tachyon <sonicgames772@gmail.com>
2026-03-13 12:51:16 +0000

    camera: add cam2api
    Signed-off-by: skye-tachyon <sonicgames772@gmail.com>

--- product/camera.mk
+++ product/camera.mk
@@ -1,7 +1,7 @@
 # Camera packages
 PRODUCT_PACKAGES += \
     camera.qcom \
-    Snap
+    libmm-qcamera
 
 # Camera SHIM packages
 PRODUCT_PACKAGES += \
@@ -16,4 +16,14 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     camera2.portability.force_api=1 \
     media.stagefright.legacyencoder=true \
-    media.stagefright.less-secure=true
+    media.stagefright.less-secure=true \
+    camera.display.lmax=1280x720 \
+    camera.display.umax=1920x1080 \
+    camera.lowpower.record.enable=1 \
+    media.camera.ts.monotonic=1 \
+    persist.camera.gyro.disable=0 \
+    persist.camera.isp.clock.optmz=0 \
+    persist.camera.stats.test=5 \
+    persist.vendor.qti.telephony.vt_cam_interface=1 \
+    vidc.enc.dcvs.extra-buff-count=2 \
+    persist.camera.HAL3.enabled=1
