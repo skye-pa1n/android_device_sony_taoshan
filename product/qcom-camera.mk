57d755f812e8e7f692cca7c2d03e2c5635aacf70
skye-tachyon <sonicgames772@gmail.com>
2026-03-13 13:31:07 +0000

    camera: re-config
    Signed-off-by: skye-tachyon <sonicgames772@gmail.com>

--- product/qcom-camera.mk
+++ product/qcom-camera.mk
@@ -1,3 +1,7 @@
+# Camera configurations
+TARGET_PROVIDES_CAMERA_HAL := true
+USE_DEVICE_SPECIFIC_CAMERA := true
+
 # Camera permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
@@ -5,7 +9,10 @@ PRODUCT_COPY_FILES += \
 
 # Camera HIDL interfaces
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl-legacy
+    android.hardware.camera.provider@2.4-impl-legacy \
+    android.hardware.camera.provider@2.4-service-lazy \
+    camera.device@1.0-impl-legacy \
+    libstagefright-shim
 
 # Disable camera Treble path
 PRODUCT_PROPERTY_OVERRIDES += \
